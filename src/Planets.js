f5c2ab213ef73b3a76d8c1e1c3689b0f1f67ab83
Jaisree Ramesh <jaisree.ramesh@gmail.com>
2022-01-13 15:57:08 +0100

    Finished with carousel

--- src/Planets.js
+++ src/Planets.js
@@ -17,10 +17,24 @@ import Urus from "./../src/Data/Planets/Urus.png";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+const name = {
+    Bairkan,
+    Jer,
+    Scloop,
+    Faihu,
+    Horst,
+    Kusma,
+    Lagus,
+    Manna,
+    Raida,
+    Urus
+  }
+
 function Planets() {
-  const [planets, setPlanets] = useState([]);
-  const [isLoading, setIsLoading] = useState([true]);
-  const [select, setSelect] = useState([]);
+    const [planets, setPlanets] = useState([]);
+    const [isLoading, setIsLoading] = useState([true])
+    const [select, setSelect] = useState({});
+    const [show, setShow] = useState( (false))
 
   useEffect(() => {
     fetch("https://space-crew.herokuapp.com/planets")
@@ -32,48 +46,58 @@ function Planets() {
       });
   }, []);
 
-  return (
-    <div className="planetsCaro">
-      {isLoading ? (
-        <div>is loading...</div>
-      ) : (
-        <Carousel>
-          {planets.map((planet, index) => (
-            <Carousel.Item key={index}>
-              <Row>
-                <img
-                  className="testimonialImages d-block w-100"
-                  src={Scloop}
-                  //  src={` {planet.Planet} `}
-
-                  alt="Bairkan"
-                  //  onClick={ setSelect(planet)}
-                />
-              </Row>
-              <Row className="m-5  justify-content-md-center planetInfo">
-                <h4>Planet {planet.Planet}</h4>
-                <br />
-                <Row>
-                  <h6>Solar System : {planet.SolarSystem}</h6>
-                  <h6>Location X : {planet.LocationX}</h6>
-                  <h6>Location Y: {planet.LocationY}</h6>
-                  <h6>Location Z: {planet.LocationZ}</h6>
-                  <h6>Vegetation: {planet.Vegetation}</h6>
-
-                  <h6>Resources: {planet.Resources}</h6>
-                  <h6>Alien-Life: {planet.AlienLife}</h6>
-                  <h6>Climatehostility: {planet.ClimateHostility}</h6>
-                  <h6>Suitability: {planet.Suitability}</h6>
-                  <h6>SuitabilityControl: {planet.SuitabilityControl}</h6>
-                </Row>
-              </Row>
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      )}
-      {/* { console.log(select)}  */}
-    </div>
-  );
+    console.log(name);
+     console.log(select)  
+    return (
+        <div>
+        <div className=" selection " style =  {{ visibility: show ? "visible" : "hidden" }}> 
+            <h4>You selected Planet {select.Planet}</h4> 
+        </div>
+            <div className="planetsCaro mt-3">
+                {isLoading ? (
+                    <div>is loading...</div>
+                ) : (
+                    <Carousel 
+                    >
+                        {planets.map( (planet, index) => (
+                            <Carousel.Item key={index}>
+                                <Row  style = {{cursor: 'pointer'}} >
+                                    <img
+                                    className="testimonialImages d-block w-100"
+                                    src={name[planet.Planet]}
+                                    alt='Bairkan'
+                                    onClick={ () => {
+                                        setSelect(planet) 
+                                        setShow(true);
+                                    }}
+                                    />
+                                </Row>
+                                <Row className = "m-5  justify-content-md-center planetInfo">
+                                    <h4>Planet {planet.Planet}</h4>
+                                    <br />
+                                    <Row >
+                                        
+                                            <h6>Solar System : {planet.SolarSystem}</h6>
+                                            <h6>Location X : {planet.LocationX}</h6>
+                                            <h6>Location Y: {planet.LocationY}</h6>
+                                            <h6>Location Z: {planet.LocationZ}</h6>
+                                            <h6>Vegetation: {planet.Vegetation}</h6>
+                                            <h6>Resources: {planet.Resources}</h6>
+                                            <h6>Alien-Life: {planet.AlienLife}</h6>
+                                            <h6>Climatehostility: {planet.ClimateHostility}</h6>
+                                            <h6>Suitability: {planet.Suitability}</h6>
+                                            <h6>SuitabilityControl: {planet.SuitabilityControl}</h6>
+                                        
+                                    </Row>
+                                </Row>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                )}
+            
+        </div>
+        </div>
+    )
 }
 
 export default Planets;
