3e13439980f67aeece469bb304d2620274fdb3e3
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-01-14 17:44:27 +0100

    fix mission alignment

--- src/Mission.js
+++ src/Mission.js
@@ -3,32 +3,37 @@ import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
+import { Col } from "react-bootstrap";
 
 function Mission() {
-  return (
-    <Container className=" mission ">
-      <Row className="p-2 mission-heading mt-5 justify-content-center">
-        <h1> SPACECREW ASSEMBLE !! </h1>
-      </Row>
-      <Row className="p-2 mission-subtext mt-5 mb-4 ">
-        As a result of an Alien invasion our Galaxy has been destroyed. Only you
-        and your crew survived the Apocalypse. As a captain of your spaceship
-        you must now decide how to populate alien planets with your skillful
-        crew and save it from extinction.
-        <br />
-        <br />
-        <br />
-        Good Luck! <br />
-      </Row>
-      <Row >
-        <NavLink to="/select">
-          <Button variant="light" type="button" className="startBtn ">
-            Start Mission!
-          </Button>
-        </NavLink>
-      </Row>
-    </Container>
-  );
+	return (
+		<Container className="mission">
+			<Row className="p-2 mission-heading mt-5 justify-content-center">
+				<h1> SPACECREW ASSEMBLE !! </h1>
+			</Row>
+
+			<Row className="p-2  mt-5 mb-4  justify-content-center">
+				<Col md={9} className="mission-subtext">
+					As a result of an Alien invasion our Galaxy has been destroyed. Only
+					you and your crew survived the Apocalypse. As a captain of your
+					spaceship you must now decide how to populate alien planets with your
+					skillful crew and save it from extinction.
+					<br />
+					<br />
+					<br />
+					Good Luck! <br />
+				</Col>
+			</Row>
+
+			<Row>
+				<NavLink to="/select">
+					<Button variant="light" type="button" className="startBtn ">
+						Start Mission!
+					</Button>
+				</NavLink>
+			</Row>
+		</Container>
+	);
 }
 
 export default Mission;
