9efc3637220034312d25ca15ef93666dd41a8622
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-01-20 11:43:34 +0100

    set up deployment dependencies

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Mission from './Mission';
-import Select from './Select';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Mission from "./Mission";
+import Select from "./Select";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <App /> } />
-        <Route path="mission" element={ <Mission /> } />
-        <Route path="select" element={ <Select /> } />
-
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<HashRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="mission" element={<Mission />} />
+				<Route path="select" element={<Select />} />
+			</Routes>
+		</HashRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
-
